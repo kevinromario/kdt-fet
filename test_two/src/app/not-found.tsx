c68f9cdc3c26a4e7a8e4067b18d9d476a3a67fac
kevinromario <holakevinromario@gmail.com>
2025-04-25 00:44:03 +0700

    feat: create not found page

--- test_two/src/app/not-found.tsx
+++ test_two/src/app/not-found.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
+/* eslint-disable @next/next/no-html-link-for-pages */
 
 export default function NotFound() {
   return (
     <div className="not-found">
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Link href="/" className="back-home">
-        Back to Home
-      </Link>
+      <a href="/">
+        <div className="back-home">Back to Home</div>
+      </a>
     </div>
   );
 }
