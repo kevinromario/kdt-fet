0e96a6abbc7c3716eee15b207eb18e4e0fb047c7
kevinromario <holakevinromario@gmail.com>
2025-04-25 00:57:21 +0700

    feat: responsive navbar

--- test_two/src/app/components/header/header.tsx
+++ test_two/src/app/components/header/header.tsx
@@ -15,12 +15,18 @@ export const Header = async () => {
         </a>
       </div>
       <div className="nav-right">
-        <a href="/about">ABOUT</a>
-        <a href="/pricing">PRICING</a>
-        <a href="/contact">CONTACT</a>
-        <a href="/login" className="login-btn">
-          LOGIN
-        </a>
+        <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+        <label htmlFor="menu-toggle" className="menu-icon">
+          ☰
+        </label>
+        <div className="nav-links">
+          <a href="/about">ABOUT</a>
+          <a href="/pricing">PRICING</a>
+          <a href="/contact">CONTACT</a>
+          <a href="/login" className="login-btn">
+            LOGIN
+          </a>
+        </div>
       </div>
     </nav>
   );
